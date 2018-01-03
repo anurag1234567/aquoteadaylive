4070415bf134b21c7ec133c9bbb5ef3fbe2be0eb
Anurag Gupta <M1034371@mindtree.com>
2018-01-03 15:14:43 +0530

    commited

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ app.get('/',function(req,res){
     res.sendfile(__dirname + "/index.html");
 });
 
-var port=process.env.port || 8085;
+var port=process.env.port || 1080;
 
 var server=app.listen(port,function(req,res){
     console.log("server started at port 8080");
