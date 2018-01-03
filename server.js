94535d626e7c9fb16b89a6c0190ab86199de7abc
Anurag Gupta <M1034371@mindtree.com>
2018-01-03 14:31:51 +0530

    Made Initial Commit

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+app.get('/',function(req,res){
+    res.sendfile(__dirname + "/index.html");
+});
+
+var port=process.env.port || 8080;
+
+var server=app.listen(port,function(req,res){
+    console.log("server started at port 8080");
+});
