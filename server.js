bc564a3b0b0437616e2f31b8bf210f53d72f0c7f
anurag1234567 <mailanurag15@gmail.com>
2018-01-04 14:32:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ app.get('/',function(req,res){
     res.sendfile(__dirname + "/index.html");
 });
 
-var port=process.env.port || 3000;
+var portnumber=process.env.Port || 3000;
 
-var server=app.listen(port,function(req,res){
+var server=app.listen(portnumber,function(req,res){
     console.log("server started at port 3000");
 });
